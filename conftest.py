83985aef8d799887a50c894a088bf14d7612e4de
Matt Brown <matt@mdbrown.dev>
2021-09-14 20:21:03 -0700

    Add 401k regression test for vanguard importer.

---
+++ conftest.py
@@ -0,0 +1,4 @@
+# This adds the --generate option for
+# https://github.com/beancount/beancount/blob/master/beancount/ingest/regression_pytest.py
+# pylint: disable=invalid-name
+pytest_plugins = "beancount.ingest.regression_pytest"
